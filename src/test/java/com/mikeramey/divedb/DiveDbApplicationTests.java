e7591733d4e5ac3e1cb1132075bec70de6ae6741
Michael Ramey <mike.ramey@gmail.com>
2018-10-15 12:28:14 -0500

    first commit

---
+++ src/test/java/com/mikeramey/divedb/DiveDbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mikeramey.divedb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiveDbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
