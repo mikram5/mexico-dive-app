b16f77c05a6d1f3b65840c58340a544e136c8a82
Michael Ramey <mike.ramey@gmail.com>
2018-10-30 16:19:08 -0500

    deleted unused test

--- src/main/java/com/mikeramey/divedb/presentation/api/AuthenticationController.java
+++ src/main/java/com/mikeramey/divedb/presentation/api/AuthenticationController.java
@@ -1,17 +1,11 @@
 package com.mikeramey.divedb.presentation.api;
 
-import com.mikeramey.divedb.logic.model.Dive;
 import com.mikeramey.divedb.logic.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import sun.net.www.protocol.http.AuthScheme;
-import sun.tools.jstat.Token;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/authentication")
