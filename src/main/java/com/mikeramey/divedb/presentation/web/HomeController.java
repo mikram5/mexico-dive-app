f233a7cb8cc0a7222a072c93b9f1981103eb36ab
Michael Ramey <mike.ramey@gmail.com>
2019-05-28 15:39:01 -0500

    build login html page

--- src/main/java/com/mikeramey/divedb/presentation/web/HomeController.java
+++ src/main/java/com/mikeramey/divedb/presentation/web/HomeController.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.time.LocalDate;
-
 @Controller
 public class HomeController {
 
